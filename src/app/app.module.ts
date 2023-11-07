5fdff9410b02d17951e61b4ec681fe8e4f7ae25e
alaahelmyoraby <alaa.helmy.oraby@gmail.com>
2023-11-08 01:49:13 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RatebarComponent } from './ratebar/ratebar.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    RatebarComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
